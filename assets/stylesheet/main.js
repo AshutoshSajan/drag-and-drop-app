019c7b79db6f838db34841758c0f754e13f0f25f
sajan <sajanloveu123@gmail.com>
2019-02-06 18:03:07 +0530

    drag and drop app complete

--- assets/stylesheet/main.js
+++ assets/stylesheet/main.js
@@ -2,7 +2,10 @@ var todo = document.querySelector(".todo-sec");
 var ul = document.querySelector(".todo-list");
 var input = document.querySelector(".main-input");
 var addBtn = document.querySelector(".add-btn");
-
+var dragElmData = null;
+// ===========================================================
+// main array function
+// ===========================================================
 var taskData = [{todoText: "JavaScript",
 status: false}, {todoText: "CSS",
 status: false},{todoText: "Drag and drop",
@@ -12,26 +15,110 @@ status: false},{todoText: "Node",
 status: false}
 ];
 
+// ===========================================================
+// object cretion and pushing it into an array function
+// ===========================================================
 function tasks(){
-	console.log("hello");
-	var taskObject = {
-		todoText: input.value,
-		status: false
+	if(input.value.trim()){
+		var taskObject = {
+			todoText: input.value,
+			status: false
+		}
+		taskData.push(taskObject);
+		input.value = "";
+		displayTask(taskData);
 	}
-	taskData.push(taskObject);
-	input.value = "";
-	displayTask(taskData);
 } 
 displayTask(taskData);
-addBtn.addEventListener("click",tasks);
 
+// ===========================================================
+// enter click function
+// ===========================================================
+addBtn.addEventListener("click",tasks);
+function enterClk(e){
+	if (e.keyCode === 13) {
+		tasks();
+	}
+}
+input.addEventListener("keydown", enterClk);
+
+// ===========================================================
+// handleDragStart function ******all DnD functions
+// ===========================================================
+function	handleDragStart(e){
+	e.target.style.opacity = '0.5';
+	
+  dragElmData = e.target;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', e.target.innerHTML);
+}
+
+// ===========================================================
+// handleDragEnter function
+// ===========================================================
+function	handleDragEnter(e){
+	e.target.classList.add("over");
+}
+
+// ===========================================================
+// handleDragOver function
+// ===========================================================
+function handleDragLeave(e) {
+  e.stopPropagation();
+  e.target.classList.remove('over');
+}
+// ===========================================================
+// handleDragOver function
+// ===========================================================
+function	handleDragOver(e){
+	if (e.preventDefault) {
+    e.preventDefault();
+  }
+  
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+// ===========================================================
+// handleDrop function
+// ===========================================================
+function	handleDrop(e){
+  if (dragElmData != e.target) {
+    dragElmData.innerHTML = e.target.innerHTML;
+    e.target.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  e.target.style.opacity = '1';
+  return false;
+}
+
+// ===========================================================
+// handleDragEnd function
+// ===========================================================
+function	handleDragEnd(e){
+	var elm = document.querySelectorAll("ul li");
+	elm.forEach(val => val.classList.remove("over"));
+	e.target.style.opacity = '1';
+}
+
+// ===========================================================
+// display function
+// ===========================================================
 function displayTask(arr){
 	ul.innerHTML = "";
-	arr.forEach(val => {
+	arr.forEach((val, index) => {
 		var li = document.createElement("li");
+		li.addEventListener('dragstart', handleDragStart, false);
+		li.addEventListener('dragenter', handleDragEnter, false)
+		li.addEventListener('dragover', handleDragLeave, false);
+		li.addEventListener('dragover', handleDragOver, false);
+		li.addEventListener('drop', handleDrop, false);
+		li.addEventListener('dragend', handleDragEnd, false);
+
 		li.classList.add("todoTask");
-		// li.innerText = val.todoText;
+		li.setAttribute('draggable', true);
+		li.setAttribute("data-id", index);
 		li.innerHTML = `<span>${val.todoText}</span> <span class="drag-txt">Drag me</span>`;
+		
 		ul.appendChild(li);
 	});
 }
